75fd07aea9507653d8561e350e79e81c35042f28
Fabio Anzola <if24b001@technikum-wien.at>
2025-01-16 15:21:54 +0100

    Show correct username in header

--- packages/mo-frontend/src/app/components/header/profile/profile.tsx
+++ packages/mo-frontend/src/app/components/header/profile/profile.tsx
@@ -1,7 +1,11 @@
+import { COOKIE_KEY_USER_TOKEN } from "@/app/utils/authUtils";
+import useDecodedToken from "../../hooks/useJwt";
+
 export function Profile() {
+  const decodedUser = useDecodedToken(COOKIE_KEY_USER_TOKEN);
   return (
     <div>
-      <span>Welcome back, Leo</span>
+      <span>Welcome back, {decodedUser?.username}</span>
     </div>
   );
 }
